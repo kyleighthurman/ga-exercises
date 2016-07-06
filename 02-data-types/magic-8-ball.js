09db77fa099ac0f7d9aec2223662d8835d423d80
Kyleigh Thurman <kyleigh.e.thurman@gmail.com>
2016-07-05 21:09:01 -0500

    magic 8-ball unfinished

---
+++ 02-data-types/magic-8-ball.js
@@ -0,0 +1,24 @@
+
+// array that will store all the responses of an 8 ball
+var randomResponses = ['It is certain',
+'It is decidedly so',
+'Without a doubt',
+'Yes, definitely',
+'You may rely on it',
+'As I see it, yes',
+'Most likely',
+'Outlook good',
+'Yes',
+'Signs point to yes',
+'Reply hazy try again',
+'Ask again later',
+'Better not tell you now',
+'Cannot predict now',
+'Concentrate and ask again',
+'Dont count on it',
+'My reply is no',
+'My sources say no',
+'Outlook not so good',
+'Very doubtful'
+];
+console.log(randomResponses[Math.floor(Math.random) * randomResponses.length]
