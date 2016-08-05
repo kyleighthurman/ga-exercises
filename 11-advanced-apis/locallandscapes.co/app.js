bd5066154868132b2bc60ea012676c935a238828
Kyleigh Thurman <kyleigh.e.thurman@gmail.com>
2016-08-05 13:32:16 -0500

    local landscapes project

---
+++ 11-advanced-apis/locallandscapes.co/app.js
@@ -0,0 +1,68 @@
+if(navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(function(pos) {
+   console.log(pos);
+   position = pos;
+ });
+} else {
+ console.warn('browser doesnt support geolocation');
+}
+
+$(document).ready(function () {
+  var $loginBtn = $('#login');
+  var $signInView = $('#longName');
+  var $imageResultsView = $('.image-results-view');
+  var $div = $('.images');
+
+  // initialize the SDK with our API key
+ _500px.init({
+   sdk_key: '8626d07001b7482e7707a6248a3b28bcaf22fd23'
+ });
+
+ _500px.getAuthorizationStatus(function (status) {
+   if (status === 'authorized') {
+     load()
+   }
+ });
+
+ $loginBtn.on('click', function() {
+   _500px.login(function (status) {
+     if(status === 'authorized') {
+       load();
+     } else {
+       console.log('not authorized');
+     }
+   });
+ });
+
+ function load() {
+
+   $signInView.hide();
+   $imageResultsView.show();
+
+   if(navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function(position) {
+       var radius = 50;
+       var geo = position.coords.latitude +
+                 ',' +
+                 position.coords.longitude +
+                 ',' +
+                 radius +
+                 'mi';
+
+       console.log(geo);
+
+       _500px.api('/photos/search', {geo: geo}, function(response) {
+         console.log(response);
+         for (var i = 0; i < response.data.photos.length; i++){
+           var url = response.data.photos[i].image_url
+           $('.images').append('<img src=' + url + '" />')
+         }
+
+
+       });
+     });
+   } else {
+     console.warn('browser doesnt support geolocation');
+   }
+ }
+});
